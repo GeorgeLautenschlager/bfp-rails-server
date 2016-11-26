5c6707afc8c9eb474657495cd22ef46a08caf6a4
George Lautenschlager <george@coupgon.com>
2016-11-26 17:45:28 -0500

    Adds bearing to ship serializer

--- app/serializers/ship_serializer.rb
+++ app/serializers/ship_serializer.rb
@@ -1,3 +1,7 @@
 class ShipSerializer < ActiveModel::Serializer
-  attributes :id, :name, :location_x, :location_y, :orientation
+  attributes :id, :name, :location_x, :location_y, :heading
+
+  def heading
+    object.orientation
+  end
 end
